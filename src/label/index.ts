b028387785d95e20beca7dd53cc8bb212dbd5495
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-06-05 21:53:49 +0200

    build(action): add dist

--- src/label/index.ts
+++ src/label/index.ts
@@ -4,14 +4,6 @@ import { GithubContextPayloadPullRequest } from "../types";
 
 export async function main(githubToken: string): Promise<void> {
   try {
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
     const octokit = getOctokit(githubToken);
     const pullRequest: GithubContextPayloadPullRequest = context.payload.pull_request;
 
@@ -20,30 +12,17 @@ export async function main(githubToken: string): Promise<void> {
       return;
     }
 
-    /**
-     * We need to fetch the list of files that were changes in the Pull Request
-     * and store them in a variable.
-     * We use octokit.paginate() to automatically loop over all the pages of the
-     * results.
-     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-     */
     const { data: changedFiles } = await octokit.rest.pulls.listFiles({
       ...context.repo,
       pull_number: pullRequest.number
     });
 
-    /**
-     * Contains the sum of all the additions, deletions, and changes
-     * in all the files in the Pull Request.
-     **/
     let diffData = {
       additions: 0,
       deletions: 0,
       changes: 0
     };
 
-    // Reference for how to use Array.reduce():
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
     diffData = changedFiles.reduce((acc, file) => {
       acc.additions += file.additions;
       acc.deletions += file.deletions;
@@ -60,11 +39,6 @@ export async function main(githubToken: string): Promise<void> {
        * Add labels according to file types.
        */
       const fileExtension = file.filename.split(".").pop();
-
-      // console.log(`
-      //           Label:
-      //           File extension : ${fileExtension}
-      //       `);
 
       switch (fileExtension) {
         case "md": {
