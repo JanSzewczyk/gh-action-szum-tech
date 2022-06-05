a1956bf7aae3f736703799698e19bff6c64964ea
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-06-05 21:57:42 +0200

    build(action): prod dependencies

--- dist/index.js
+++ dist/index.js
@@ -144,39 +144,18 @@ const core = __importStar(__nccwpck_require__(2186));
 function main(githubToken) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            /**
-             * Now we need to create an instance of Octokit which will use to call
-             * GitHub's REST API endpoints.
-             * We will pass the token as an argument to the constructor. This token
-             * will be used to authenticate our requests.
-             * You can find all the information about how to use Octokit here:
-             * https://octokit.github.io/rest.js/v18
-             **/
             const octokit = (0, github_1.getOctokit)(githubToken);
             const pullRequest = github_1.context.payload.pull_request;
             if (!pullRequest) {
                 new Error("Pull Request doesn't exists!!!");
                 return;
             }
-            /**
-             * We need to fetch the list of files that were changes in the Pull Request
-             * and store them in a variable.
-             * We use octokit.paginate() to automatically loop over all the pages of the
-             * results.
-             * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-             */
             const { data: changedFiles } = yield octokit.rest.pulls.listFiles(Object.assign(Object.assign({}, github_1.context.repo), { pull_number: pullRequest.number }));
-            /**
-             * Contains the sum of all the additions, deletions, and changes
-             * in all the files in the Pull Request.
-             **/
             let diffData = {
                 additions: 0,
                 deletions: 0,
                 changes: 0
             };
-            // Reference for how to use Array.reduce():
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
             diffData = changedFiles.reduce((acc, file) => {
                 acc.additions += file.additions;
                 acc.deletions += file.deletions;
@@ -192,10 +171,6 @@ function main(githubToken) {
                  * Add labels according to file types.
                  */
                 const fileExtension = file.filename.split(".").pop();
-                // console.log(`
-                //           Label:
-                //           File extension : ${fileExtension}
-                //       `);
                 switch (fileExtension) {
                     case "md": {
                         yield octokit.rest.issues.addLabels(Object.assign(Object.assign({}, github_1.context.repo), { issue_number: pullRequest.number, labels: ["markdown"] }));
